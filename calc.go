094eb8538f8bd8ce6210afc1132ea28dbd36de10
sLrBastos <cerejobastosa7x@gmail.com>
2023-08-21 21:26:04 +0100

    done some changes to the polish calculator. add some meme value

--- calc.go
+++ calc.go
@@ -14,12 +14,15 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter the reverse Polish expression: ")
 	expression, _ := reader.ReadString('\n')
-	result := ReversePolish(expression)
+	result, errMsg := ReversePolish(expression)
+	if errMsg != "" {
+		fmt.Println("Error:", errMsg)
+	}
 	fmt.Printf("Result: %d\n", result)
 
 }
 
-func ReversePolish(expression string) int {
+func ReversePolish(expression string) (int, string) {
 	tokens := strings.Fields(expression)
 	stack := &stack.Stack{}
 
@@ -27,24 +30,29 @@ func ReversePolish(expression string) int {
 		if isOperator(token) {
 			operand2 := stack.Pop()
 			operand1 := stack.Pop()
+
+			if token == "/" && operand2 == 0 {
+				illegal := "WHAT YOU'VE JUST DONE IS ILLEGAL! I WILL SEE YOU IN THE COURT OF LAW"
+				return 0, illegal
+			}
+
 			result := performOperation(token, operand1, operand2)
 			stack.Push(result)
 		} else {
 			operand, err := strconv.Atoi((token))
 			if err != nil {
-				fmt.Printf("Error parsing operand: %s\n", err)
-				return 0
+				return 0, fmt.Sprint("Error parsing operand: %s\n", err)
 			}
 			stack.Push(operand)
 		}
 	}
-
+	// AT THE END OF THE LOOP, THERE SHOULD BE 1 ELEMENT IN THE STACK WHICH IS THE RESULT OF THE OPERATION.
+	// IF THERE MORE OR LESS THAN 1 SOMETHING WENT WRONG AND THE EXPRESSION MIGHT BE INVALID
 	if stack.Size() != 1 {
-		fmt.Println("Invalid expression")
-		return 0
+		return 0, "Invalid expression"
 	}
 
-	return stack.Pop()
+	return stack.Pop(), ""
 }
 
 func isOperator(token string) bool {
