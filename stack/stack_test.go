6ee9813d6e75b21c478f1111b732dc9ff5207052
sLrBastos <cerejobastosa7x@gmail.com>
2023-08-21 22:17:23 +0100

    fixed issue in the stack testing

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -5,8 +5,8 @@ import "testing"
 // TESTING STACK. SEEING IF PUSH, BY PUSHING 5, AND POP BY SEEING IF IT POPS THE 5 THAT WAS JUST PUSHED
 func TestPushPop(t *testing.T) {
 	c := new(Stack)
-	c.push(5)
-	if c.pop() != 5 {
+	c.Push(5)
+	if c.Pop() != 5 {
 		t.Log("Pop should have been 5")
 		t.Fail()
 	}
